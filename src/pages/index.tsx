036a827d88daf0fd548dabe48b326b9889593c32
Iván González Martínez <ivan.gonzalez@sage.com>
2023-04-20 10:43:34 +0200

    feat: add vercel analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
 import Home from "../components/Home";
+import { Analytics } from "@vercel/analytics/react";
 
 const HomePage: NextPage = () => {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Analytics />
+    </>
+  );
 };
 
 export default HomePage;
